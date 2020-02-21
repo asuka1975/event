46a43a01123780ef2a01ab5918c163a00870fd1c
hungr <shikina.asuka@i.mbox.nagoya-u.ac.jp>
2020-02-22 02:18:05 +0900

    create property.h

--- include/event.h
+++ include/event.h
@@ -9,6 +9,7 @@
 #include <list>
 #include <functional>
 #include <mutex>
+#include <stdexcept>
 
 template <class TSender, class TEventArgs>
 class event {
@@ -24,7 +25,7 @@ public:
     void operator=(event<TSender, TEventArgs>&& obj) = delete;
 
 private:
-    using callback_type = void(*)(TSender, TEventArgs);
+    using fp_type = void(*)(TSender, TEventArgs);
     std::mutex mtx;
     std::list<std::function<void(TSender, TEventArgs)>> callbacks;
 };
@@ -32,8 +33,11 @@ private:
 template <class TSender, class TEventArgs>
 inline void event<TSender, TEventArgs>::operator+=(std::function<void(TSender, TEventArgs)> callback) {
     std::lock_guard<std::mutex> lock(mtx);
-    auto citer = std::find_if(callbacks.begin(). callbacks.end(), [&callback](auto c) {
-        return *(callback.target()) == *(c.target());
+    auto citer = std::find_if(callbacks.begin(), callbacks.end(), [&callback](auto c) {
+        auto fp1 = callback.template target<fp_type>();
+        auto fp2 = callback.template target<fp_type>();
+        if(fp1 == nullptr || fp2 == nullptr) throw std::runtime_error("invalid function signature. type `void(*)(TSender, TEventArgs)` is required");
+        return *(callback.template target<fp_type>()) == *(c.template target<fp_type>());
     });
     if(citer == callbacks.end()) callbacks.push_back(callback);
 }
@@ -41,10 +45,14 @@ inline void event<TSender, TEventArgs>::operator+=(std::function<void(TSender, T
 template<class TSender, class TEventArgs>
 inline void event<TSender, TEventArgs>::operator-=(std::function<void(TSender, TEventArgs)> callback) {
     std::lock_guard<std::mutex> lock(mtx);
-    auto citer = std::find_if(callbacks.begin(). callbacks.end(), [&callback](auto c) {
-        return *(callback.target()) == *(c.target());
+    auto citer = std::find_if(callbacks.begin(), callbacks.end(), [&callback](auto c) {
+        auto fp1 = callback.template target<fp_type>();
+        auto fp2 = callback.template target<fp_type>();
+        if(fp1 == nullptr || fp2 == nullptr) throw std::runtime_error("invalid function signature. type `void(*)(TSender, TEventArgs)` is required");
+        return *(callback.template target<fp_type>()) == *(c.template target<fp_type>());
     });
     if(citer != callbacks.end()) callbacks.erase(citer);
+
 }
 
 template<class TSender, class TEventArgs>
@@ -52,6 +60,5 @@ inline void event<TSender, TEventArgs>::operator()(TSender sender, TEventArgs e)
     std::lock_guard<std::mutex> lock(mtx);
     for(auto callback : callbacks) callback(sender, e);
 }
-
 
 #endif //EVENT_EVENT_H
